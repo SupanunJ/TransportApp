8ef6de6c7558a7f9b7efabc4211e6118ed5a35a3
Nattanicha76 <33824068+Nattanicha76@users.noreply.github.com>
2018-07-20 15:13:47 +0700

    sumbill

--- components/AppTabNavigator/DetailTab/DetailBill.js
+++ components/AppTabNavigator/DetailTab/DetailBill.js
@@ -1,109 +1,176 @@
 import React, { Component } from 'react'
 import { Text, StyleSheet, StatusBar, Alert, View, Platform, Image, Dimensions, ScrollView } from 'react-native'
 
-import { Icon, Container, Header, Left, Body, Title, Right, Button, Content, Footer,Separator ,ListItem} from 'native-base';
-
+import { Icon, Container, Header, Left, Body, Title, Right, Button, Content, Footer, Separator, ListItem } from 'native-base';
+import { gql, withApollo, compose } from 'react-apollo'
 class DetailBill extends Component {
 
-    render() {
-
-        const { navigate } = this.props.navigation
-
-        return (
-            
-            <Container style={{ backgroundColor: 'white'}}>
-                <Header style={{ backgroundColor: '#66c2ff' }}>
-                    <Left>
-                        <Button transparent
-                            onPress={() => navigate('SumBill')}>
-                            <Icon name='arrow-back' />
-                        </Button>
-                    </Left>
-                    <Body>
-                        <Title>รายละเอียดยอดเงิน</Title>
-                    </Body>
-                    <Right />
-                </Header>
-
-                <Content>
-              <Separator bordered>
-                <Text style={styles.storeLabel}>ABCD001</Text>
-              </Separator>
-
-              <ListItem  style={{   backgroundColor: 'white', borderColor: 'white', paddingLeft: 0}} >
-             
-              <View>
-                  <View style={{ paddingLeft: 10, flexDirection: 'row' }}>
-                  <Text style={{ paddingHorizontal:5 }}></Text>
-                    <Text style={{ paddingHorizontal:5 }}>ฟิล์มกระจก</Text>
-                    <Text style={{ paddingHorizontal:30 }}></Text>
-                    <Text style={{ paddingHorizontal:5 }}>20</Text>
-            
-                    <Text style={{ paddingHorizontal: 5 }}>ชิ้น</Text>
-                  </View>
-                </View>
-                <View style={{ position: 'absolute', right: 5 , flexDirection: 'row' }}>
-                  
-                    <Text style={{ paddingHorizontal: 5 }}>2500</Text>
-                    <Text style={{ paddingHorizontal: 5 }}>บาท</Text>
-                
-                </View>
-              </ListItem>
-              
-              <Separator bordered>
-                <Text style={styles.storeLabel}>ABCD002</Text>
-              </Separator>
-              <ListItem>
-                <View>
-                  <View style={{ paddingLeft: 10, flexDirection: 'row' }}>
-                  <Text style={{ paddingHorizontal:5 }}></Text>
-                    <Text style={{ paddingHorizontal:5 }}>ฟิล์มกระจก</Text>
-                    <Text style={{ paddingHorizontal:30 }}></Text>
-                    <Text style={{ paddingHorizontal:5 }}>20</Text>
-            
-                    <Text style={{ paddingHorizontal: 5 }}>ชิ้น</Text>
-                  </View>
-                </View>
-                <View style={{ position: 'absolute', right: 0 , flexDirection: 'row' }}>
-                  
-                    <Text style={{ paddingHorizontal: 5 }}>2500</Text>
-                    <Text style={{ paddingHorizontal: 5 }}>บาท</Text>
-                
-                </View>
-              </ListItem>
-            </Content>
-
-              
-            </Container>
-
-        )
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDetailBill: [],
+      showinvoicebill: []
     }
+    // this.props.client.resetStore();
+    this.detailsummoney();
+    this.checkinvoicebill();
+  }
+
+  detailsummoney = () => {
+    console.log("detailsummoney")
+
+    this.props.client.query({
+      query: detailsummoney,
+      variables: {
+        "MessengerID": global.NameOfMess
+      }
+    }).then((result) => {
+      console.log(result.data.detailsummoney)
+      this.setState({
+        showDetailBill: result.data.detailsummoney
+      })
+    }).catch((err) => {
+      console.log(err)
+    });
+  }
+
+  checkinvoicebill = () => {
+    console.log("queryZone")
+
+    this.props.client.query({
+      query: checkinvoicebill,
+      variables: {
+        "MessengerID": global.NameOfMess
+      }
+    }).then((result) => {
+      console.log(result.data.checkinvoicebill)
+      this.setState({
+        showinvoicebill: result.data.checkinvoicebill
+      })
+    }).catch((err) => {
+      console.log(err)
+    });
+  }
+  render() {
+
+    const { navigate } = this.props.navigation
+
+    return (
+
+      <Container style={{ backgroundColor: 'white' }}>
+        <Header style={{ backgroundColor: '#66c2ff' }}>
+          <Left>
+            <Button transparent
+              onPress={() => navigate('SumBill')}>
+              <Icon name='arrow-back' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>รายละเอียดยอดเงิน</Title>
+          </Body>
+          <Right />
+        </Header>
+
+        <Content>
+
+          <View>
+            {
+              this.state.showinvoicebill.map(val => (
+                <View>
+                  <Separator bordered>
+                    <Text style={styles.storeLabel}>  {"  "}{val.invoiceNumber}</Text>
+                  </Separator>
+
+                  <View>
+                    {
+                      this.state.showDetailBill.map(l => {
+                        if (l.invoiceNumber == val.invoiceNumber) {
+                          return (
+                            <View style={styles.detailContent}>
+                              <ListItem style={{ backgroundColor: 'white', borderColor: 'white', paddingLeft: 0 }} >
+
+                                <View style={{ width: Dimensions.get('window').width / 3, justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
+                                  <Text >{l.itemName}</Text>
+                                  </View>
+                                  <View style={{ width: Dimensions.get('window').width / 3, justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
+                                  <Text >{l.qty}</Text>
+                                 <Text >ชิ้น</Text>
+                                 </View>
+                                  <View style={{ width: Dimensions.get('window').width / 3, justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
+                                  <Text >{l.amount}</Text>
+                                  
+                                  <Text >บาท</Text>
+
+                                </View>
+                              </ListItem>
+                            </View>
+
+                          )
+                        }
+                      })
+                    }
+                  </View>
+                </View>
+
+
+              )
+              )
+            }
+
+          </View>
+        </Content>
+
+
+      </Container>
+
+    )
+
+  }
 }
 
 
-export default DetailBill;
+const GraphQL = compose(DetailBill)
+export default withApollo(GraphQL)
+
+const detailsummoney = gql`
+query detailsummoney($MessengerID:String!){
+  detailsummoney(MessengerID: $MessengerID){
+    invoiceNumber
+    qty
+    amount
+    itemName
+    
+  }
+}
+`
+const checkinvoicebill = gql`
+  query checkinvoicebill($MessengerID:String!){
+    checkinvoicebill(MessengerID: $MessengerID){
+      invoiceNumber
+      }
+  }
+`
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection: 'row',
-      alignItems: "center",
-      justifyContent: "center"
-    },
-    storeLabel: {
-      fontSize: 18,
-      color: 'black'
-    },
-    detailContent: {
-      width: Dimensions.get('window').width,
-      backgroundColor: 'white',
-      borderColor: 'white',
-      borderRightWidth: 2,
-      borderLeftWidth: 2,
-      borderTopWidth: 1,
-      borderBottomWidth: 1,
-      height: 50,
-      justifyContent: 'center'
-    }
-  })
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  storeLabel: {
+    fontSize: 18,
+    color: 'black'
+  },
+  detailContent: {
+    width: Dimensions.get('window').width,
+    backgroundColor: 'white',
+    borderColor: 'white',
+    borderRightWidth: 2,
+    borderLeftWidth: 2,
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    height: 50,
+    justifyContent: 'center'
+  }
+})
--- components/AppTabNavigator/DetailTab/SumBill.js
+++ components/AppTabNavigator/DetailTab/SumBill.js
@@ -2,17 +2,115 @@ import React, { Component } from 'react'
 import { Text, StyleSheet, StatusBar, Alert, View, Platform, Image, Dimensions, ScrollView, TouchableOpacity } from 'react-native'
 
 import { Icon, Container, Header, Left, Body, Title, Right, Button, Content, Footer, Input, Item, Grid, Col } from 'native-base';
+import { gql, withApollo, compose } from 'react-apollo'
 
 class SumBill extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            showsummoney: [],
+            showStatusreport: [],
+            showinvoicedetail_ID: [],
+        }
+        this.props.client.resetStore();
+        this.summoney();
+    }
+
+    summoney = () => {
+        console.log("summoney")
+
+        this.props.client.query({
+            query: summoney,
+            variables: {
+                "MessengerID": global.NameOfMess
+            }
+        }).then((result) => {
+            console.log(result.data.summoney)
+            this.setState({
+                showsummoney: result.data.summoney
+            })
+        }).catch((err) => {
+            console.log(err)
+        });
+    }
+    checkinvoicereport = () => {
+        const { navigate } = this.props.navigation
+        this.props.client.query({
+            query: checkinvoicereport,
+            variables: {
+                "MessengerID": global.NameOfMess
+            }
+        }).then((result) => {
+            console.log("checkinvoicereport", result.data.checkinvoicereport)
+            this.setState({ showinvoicedetail_ID: result.data.checkinvoicereport })
+            console.log("NUM",this.state.showinvoicedetail_ID.length)
+            if(this.state.showinvoicedetail_ID.length > 0)
+            {
+                this.reportsubmitwork();
+            }else{
+                navigate('SumBill')
+            }
+            
+        }).catch((err) => {
+            console.log("err of checkinvoicereport", err)
+        });
+    }
+
+
+    reportsubmitwork = () => {
+        console.log("reportsubmitwork")
+        this.props.client.mutate({
+            mutation: reportsubmitwork,
+            variables: {
+                "MessengerID": global.NameOfMess
+            }
+        }).then((result) => {
+            console.log(global.NameOfMess)
+            console.log("result", result.data.reportsubmitwork)
+            this.state.showinvoicedetail_ID.map(l => (
+                this.reportdetail(l.invoiceNumber)
+            ));
+        }).catch((err) => {
+            console.log("error of reportsubmitwork", err)
+        });
+    }
+
+    reportdetail = (id) => {
+        const { navigate } = this.props.navigation
+        console.log("reportdetail")
+
+        this.props.client.mutate({
+            mutation: reportdetail,
+            variables: {
+                "invoiceNumber": id
+            }
+        }).then((result) => {
+            console.log("...")
+            navigate('MainMenu')
+        }).catch((err) => {
+            console.log("error", err)
+        });
+    }
+    _PRESS_SearchTab = () => {
+        // const { navigate } = this.props.navigation
+        this.checkinvoicereport();
+        // this.billTOapp();
+
+        // this.state.showINVOICE_ID.map(l => (
+        //     this.detailtoapp(l.INVOICEID)
+        // ));
+        // navigate('HomeTab')
+    }
 
     static navigationOptions = {
         header: null
     }
-  render() {
+    render() {
 
-    const { navigate } = this.props.navigation
+        const { navigate } = this.props.navigation
 
-    return (
+        return (
 
             <Container>
                 <Header style={{ backgroundColor: '#66c2ff' }}>
@@ -29,56 +127,72 @@ class SumBill extends Component {
                 </Header>
 
                 <Content>
+                    <View>
+                        {
+                            this.state.showsummoney.map((l, i) => (
+                                <View>
+                                    <View style={{ margin: 30, justifyContent: 'center' }}>
+                                        <Text>สรุปยอดเงินที่ต้องโอน  </Text>
+                                    </View>
 
-                    <View style={{ margin: 10 }}>
-                        <Text>รหัสบิล : </Text>
-                        <Text>ห้าง : </Text>
+                                    <View style={{ margin: 45, marginTop: 5, justifyContent: 'center' }}>
+                                        <Text>ยอดงเงินตามบิลจริง :                {l.amountBill}           บาท</Text>
+                                        <Text>ยอดเงินที่เก็บได้ :                     {l.amountActual}         บาท </Text>
+                                        <Text>ยอดที่เก็บไม่ได้ :                      {l.amountBill - l.amountActual}         บาท </Text>
+                                    </View>
+                                    <View style={{ margin: 30, marginTop: 5, justifyContent: 'center' }}>
+                                        <Text>ยอดเงินที่ต้องโอนเข้าบัญชีของบริษัท    {l.amountActual}     บาท</Text>
+                                    </View>
+
+                                    <View style={{ margin: 30, marginTop: 5, justifyContent: 'center' }}>
+                                        <Text>หมายเลขบัญชี 476-800-083-7  </Text>
+                                        <Text>พร้อมเพย์ 099-9999-000  </Text>
+                                    </View>
+                                </View>
+                            ))
+                        }
                     </View>
 
-                    <View style={{ justifyContent: 'space-around', flexDirection: 'row' }}>
-
-                        <Text>ชื่อ</Text>
-                        <Text>จำนวน</Text>
-                        <Text>ราคา</Text>
-
-                    </View>
-
-                    <View style={{ flexDirection: 'row' }}>
-
-                        <View style={{ width: Dimensions.get('window').width / 3, justifyContent: 'center', alignItems: 'center' }}>
-                            <Text>GOAS101-APIPH8P</Text>
-                        </View>
-                        <View style={{ width: Dimensions.get('window').width / 3, justifyContent: 'center', alignItems: 'center' }}>
-                            <Text>1</Text>
-                        </View>
-                        <View style={{ width: Dimensions.get('window').width / 3, justifyContent: 'center', alignItems: 'center' }}>
-                            <Text>60</Text>
-                        </View>
-
-                    </View>
-                    <View style={{ margin: 10 }}>
-                        <Text>หมายเหตุ : </Text>
-                       
-                    </View>
 
                 </Content>
-                <Footer style={{ 
+                <Footer style={{
                     backgroundColor: '#66c2ff',
-                    justifyContent:'center', 
+                    justifyContent: 'center',
                     alignItems: 'center'
-                    }}>
-                    <View style={{ justifyContent:'center', alignItems: 'center' }}>
-                        <Button warning 
-                        onPress={() => navigate('DetailBill')} 
-                         style={{ 
-                            width: 200, 
-                            height: '80%', 
-                            justifyContent:'center', 
-                            alignItems: 'center' }}
-                        >
-                            <Text style={{ color: 'white', fontWeight: 'bold' }}>รายละเอียดยอดเงิน</Text>
-                        </Button>
-                    </View>
+                }}>
+                    <View style={{  justifyContent: 'center', alignItems: 'center' , flexDirection:'row', paddingLeft : 10}}>
+                        <View style={{ width: Dimensions.get('window').width / 2, justifyContent: 'center', alignItems: 'center', paddingLeft:5 }}>
+                            <Button success
+                                onPress={
+                                    this._PRESS_SearchTab.bind(this)
+                                }//navigate('HomeTab')
+                                style={{
+                                    width: Dimensions.get('window').width / 2.3,
+                                    height: '80%',
+                                    justifyContent: 'center',
+                                    alignItems: 'center'
+                                }}
+                            >
+                                <Text style={{ color: 'white', fontWeight: 'bold' }}>เคลียร์งาน</Text>
+                            </Button>
+                        </View>
+                        <View style={{ width: Dimensions.get('window').width / 2, justifyContent: 'center', alignItems: 'center' }}>
+                            <Button warning
+                                onPress={() => navigate('DetailBill')}
+                                style={{
+                                    width: Dimensions.get('window').width / 2.3,
+                                    height: '80%',
+                                    justifyContent: 'center',
+                                    alignItems: 'center'
+                                }}
+                            >
+                                <Text style={{ color: 'white', fontWeight: 'bold' }}>รายละเอียดยอดเงิน</Text>
+                            </Button>
+                        </View>
+             
+                        </View>
+
+
                 </Footer>
             </Container>
 
@@ -87,6 +201,52 @@ class SumBill extends Component {
     }
 }
 
-export default SumBill;
 
+const GraphQL = compose(SumBill)
+export default withApollo(GraphQL)
 
+const summoney = gql`
+query summoney($MessengerID:String!){
+  summoney(MessengerID: $MessengerID){
+    amountBill
+    amountActual
+  }
+}
+`
+
+const reportsubmitwork = gql`
+    mutation reportsubmitwork($MessengerID:String!){
+        reportsubmitwork(MessengerID: $MessengerID){
+            status
+        }
+    }
+`
+const reportdetail = gql`
+    mutation reportdetail($invoiceNumber:String!){
+        reportdetail(invoiceNumber: $invoiceNumber){
+            status
+        }
+    }
+`
+const checkinvoicereport = gql`
+    query checkinvoicereport($MessengerID:String!){
+        checkinvoicereport(MessengerID: $MessengerID){
+            invoiceNumber
+        }
+    }
+`
+
+const styles = StyleSheet.create({
+
+    detailContent: {
+        width: Dimensions.get('window').width,
+        backgroundColor: 'white',
+        borderColor: 'white',
+        borderRightWidth: 2,
+        borderLeftWidth: 2,
+        borderTopWidth: 1,
+        borderBottomWidth: 1,
+        height: 50,
+        justifyContent: 'center'
+    }
+})
